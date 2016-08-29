dcd9e8c2dbacc91b20b8d3194452538d7a225d2d
arun.s <arun.s@imaginea.com>
2016-08-29 15:33:46 +0530

    Test commit

---
+++ src/demoJenkins/JenkinsFclass.java
@@ -0,0 +1,22 @@
+package demoJenkins;
+
+import org.testng.annotations.Test;
+
+public class JenkinsFclass {
+	
+
+	@Test	
+	
+	//public static void main(String args[]) {
+		
+	public void testJenkins() {
+	
+		System.out.println("Welcome to Jenkins Demo");
+	}
+		
+		
+	}
+
+
+
+
